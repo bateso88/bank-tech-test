a7551135c0b1bbd61c71dd45231ae735498eddc6
bateso88 <bateso@hotmail.co.uk>
2021-03-08 14:32:20 +0000

    add transactions attribute to statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,14 +4,26 @@ require_relative 'transaction'
 
 # Statement class. Can create new transactions and print
 class Statement
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def update(value, transaction_class = Transaction)
+    update_balance(value)
+    transaction = debit_or_credit?(value, transaction_class)
+    transactions.unshift(transaction)
+  end
+
+  private
+
+  def update_balance(value)
     @balance += value
+  end
+
+  def debit_or_credit?(value, transaction_class)
     if value.negative?
       transaction_class.new(credit: nil, debit: -value, balance: balance)
     else
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,8 +8,6 @@ describe Statement do
   let(:transaction_class) { double('transaction_class', new: transaction) }
 
   context 'update' do
-    it { is_expected.to respond_to :update }
-
     it 'creates a new transaction after a deposit' do
       expect(transaction_class).to receive(:new).with({ balance: 100, credit: 100, debit: nil })
       statement.update(100, transaction_class)
@@ -20,9 +18,18 @@ describe Statement do
       statement.update(-100, transaction_class)
     end
 
-    it 'updates the balance' do
+    it 'updates the balance correctly' do
       statement.update(100, transaction_class)
       expect(statement.balance).to eq 100
+    end
+  end
+
+  context 'transactions' do
+    it 'shows a list of all transactions' do
+      statement.update(100, transaction_class)
+      statement.update(-50, transaction_class)
+      expect(statement.transactions.length).to eq 2
+      expect(statement.transactions).to all(eq(transaction))
     end
   end
 end
