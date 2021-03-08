b9f59b57d912b2b5e3f848cb67279f12a8cee502
bateso88 <bateso@hotmail.co.uk>
2021-03-08 12:27:38 +0000

    create a new transaction on statement.update

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,5 +2,7 @@
 
 # BankAccount class. Can withdraw from and deposit to bank account
 class Statement
-  def update; end
+  def update(value, transaction_class = Transaction)
+    transaction_class.new(value)
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,8 +4,15 @@ require 'statement'
 
 describe Statement do
   let(:statement) { Statement.new }
+  let(:transaction) { double('transaction') }
+  let(:transaction_class) { double('transaction_class', new: transaction) }
 
   context 'update' do
     it { is_expected.to respond_to :update }
+
+    it 'creates a new transaction after a deposit' do
+      expect(transaction_class).to receive(:new).with(100)
+      statement.update(100, transaction_class)
+    end
   end
 end
